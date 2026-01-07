b7eb195647e64dc1c85f81699bb501f8cff8e93b
Leon35-fc <leon.f35@alice.it>
2026-01-07 16:56:18 +0100

    Done

---
+++ untitled/src/es2/Main.java
@@ -0,0 +1,29 @@
+package es2;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println(numToWord());
+    }
+    public static String numToWord(){
+        System.out.println("Scrivi un numero da 0 a 3");
+        int num = scn.nextInt();
+        scn.nextLine(); // per consumare il newline
+
+        switch (num){
+            case 0:
+                return "zero";
+            case 1:
+                return "uno";
+            case 2:
+                return "due";
+            case 3:
+                return "tre";
+            default:
+                return "Valore errato";
+        }
+    }
+}
+
---
+++ untitled/src/es4/Main.java
@@ -0,0 +1,20 @@
+package es4;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println(contdown());
+
+    }
+    public static int contdown(){
+        String ctnInp = scn.nextLine();
+        int ctn = Integer.parseInt(ctnInp);
+        for( int i = 0; ctn >0; i++){
+            ctn--;
+            System.out.println(ctn);
+        }
+        return ctn;
+    }
+}
