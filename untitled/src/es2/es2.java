cd13c2f5b72217c23eb9089d7c1b3bdbfad69e9e
Leon35-fc <leon.f35@alice.it>
2026-01-08 02:02:46 +0100

    Update es2.java

--- untitled/src/es2/es2.java
+++ untitled/src/es2/es2.java
@@ -6,14 +6,14 @@ public class es2 {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(numToWord());
-    }
-
-    public static String numToWord() {
         System.out.println("Scrivi un numero da 0 a 3");
         int num = scn.nextInt();
-        scn.nextLine(); // per consumare il newline
+        System.out.println(numToWord(num));
+    }
 
+    public static String numToWord(int input) {
+        //  scn.nextLine(); // per consumare il newline
+        int num = input;
         switch (num) {
             case 0:
                 return "zero";
